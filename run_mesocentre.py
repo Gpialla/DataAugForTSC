f12e31a420225c5badff9b1cbdd8fec043057a71
Gautier Pialla <gautier.pialla@uha.fr>
2022-03-24 16:34:34 +0100

    Fixing run on mesocentre

--- run_mesocentre.py
+++ run_mesocentre.py
@@ -2,20 +2,20 @@ import os
 
 DS_2018 = [
 	'ACSF1', 
-#    'Adiac', 'AllGestureWiimoteX', 'AllGestureWiimoteY', 'AllGestureWiimoteZ',
-#	'ArrowHead', 'Beef', 'BeetleFly', 'BirdChicken', 'BME', 'Car', 'CBF', 'Chinatown',
-#	'ChlorineConcentration', 'CinCECGTorso', 'Coffee', 'Computers', 'CricketX',
-#	'CricketY', 'CricketZ', 'Crop', 'DiatomSizeReduction'
+    'Adiac', 'AllGestureWiimoteX', 'AllGestureWiimoteY', 'AllGestureWiimoteZ',
+	'ArrowHead', 'Beef', 'BeetleFly', 'BirdChicken', 'BME', 'Car', 'CBF', 'Chinatown',
+	'ChlorineConcentration', 'CinCECGTorso', 'Coffee', 'Computers', 'CricketX',
+	'CricketY', 'CricketZ', 'Crop', 'DiatomSizeReduction'
 ]
 
-AUG_METHODS      = ["dgw"]#, "rgw", "scaling", "windowwarp"]
-AUG_EACH_EPCH    = False             # New
+AUG_METHODS      = ["dgw", "rgw", "scaling", "windowwarp"]
+AUG_EACH_EPCH    = False             
 MULTI_AUG        = False
-MULTI_AUG_METHOD = 'MULTI'          # New
-ONLY_AUG_DATA    = True            # New
+MULTI_AUG_METHOD = 'MULTI'         
+ONLY_AUG_DATA    = True           
 EXP_NAME         = "OneAug_Aug_Only"
 NUM_ITR          = 1
-CLSSF_NAME = "inception"
+CLSSF_NAME       = "inception"
 
 EPOCHS      = 900
 BATCH_SIZE  = 64
@@ -24,7 +24,7 @@ for ds_name in DS_2018:
     if not MULTI_AUG:
         for aug in AUG_METHODS:
             for itr in range(NUM_ITR):
-                command = "sbatch sbatch-main.sh {} {} {} {} {} {} {} {} {}".format(EXP_NAME, ds_name, aug, AUG_EACH_EPCH, ONLY_AUG_DATA, MULTI_AUG_METHOD, CLSSF_NAME, EPOCHS, BATCH_SIZE, itr)
+                command = "sbatch sbatch-main.sh {} {} {} {} {} {} {} {} {} {}".format(EXP_NAME, ds_name, aug, AUG_EACH_EPCH, ONLY_AUG_DATA, MULTI_AUG_METHOD, CLSSF_NAME, EPOCHS, BATCH_SIZE, itr)
                 os.system(command)
                 print("Run command " + command)
     else:
@@ -34,6 +34,6 @@ for ds_name in DS_2018:
         aug_str=aug_str[:-1]+"'"
 
         for itr in range(NUM_ITR):
-            command = "sbatch sbatch-main.sh {} {} {} {} {} {} {} {} {}".format(EXP_NAME, ds_name, aug_str, AUG_EACH_EPCH, ONLY_AUG_DATA, MULTI_AUG_METHOD, CLSSF_NAME, EPOCHS, BATCH_SIZE, itr)
+            command = "sbatch sbatch-main.sh {} {} {} {} {} {} {} {} {} {}".format(EXP_NAME, ds_name, aug_str, AUG_EACH_EPCH, ONLY_AUG_DATA, MULTI_AUG_METHOD, CLSSF_NAME, EPOCHS, BATCH_SIZE, itr)
             os.system(command)
             print("Run command " + command)
