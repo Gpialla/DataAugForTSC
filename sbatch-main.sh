42c68023cb29815ddd652a3303d26da73b323673
Gautier Pialla <gautier.pialla@uha.fr>
2022-08-29 16:07:51 +0200

    Modifying output directory

--- sbatch-main.sh
+++ sbatch-main.sh
@@ -8,4 +8,4 @@ source activate transformers
 
 cd ~/projects/DataAugForTSC
 
-python3 main.py --exp_name $1 --archive_name $2 --archive_version $3 --ds_name $4 --aug_each_epch $5 --only_aug_data $6 --multi_aug_method $7 --model $8 --num_epochs $9 --batch_size ${10} --iter ${11}
+python3 main.py --exp_name $1 --archive_name $2 --archive_version $3 --ds_name $4 --aug_each_epch $5 --only_aug_data $6 --multi_aug_method $7 --model $8 --num_epochs $9 --batch_size ${10}
